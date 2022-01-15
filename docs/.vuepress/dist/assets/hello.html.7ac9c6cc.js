53ce09a1b85da1022fe17e0eddfd99db36119e90
zhuzhengjian <aibyanyu@qq.com>
2022-01-15 10:36:14 +0800

    fix: 删除dist

--- docs/.vuepress/dist/assets/hello.html.7ac9c6cc.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-712f2b00",path:"/hello.html",title:"hello",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1641912635e3,contributors:[{name:"zhuzhengjian",email:"aibyanyu@qq.com",commits:1}]},filePathRelative:"hello.md"};export{e as data};
