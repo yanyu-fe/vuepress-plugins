b4c1b0dc3f0c4390acd7261813fb2bf068dc26e7
aibayanyu <aibayanyu@qq.com>
2022-03-17 00:23:43 +0800

    fix: 修改bug样式混乱

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,9 +17,53 @@ export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
             link: "/",
           },
           {
-            text: "测试写一下",
-            link: "/hello",
+            text:"指南",
+            link:"/guide/"
           },
+          {
+            text:"开源",
+            children:[
+              {
+                text:"vuepress",
+                children:[
+                  {
+                    text:"代码块插件",
+                    link:"/plugins/vuepress/code-block"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            text:"博客",
+            children:[
+              {
+                text: "vue",
+                link:"/blog/vue/"
+              },
+              {
+                text: "taro",
+                link:"/blog/taro/"
+              }
+            ]
+          },
+          {
+            text:"UI库",
+            children:[
+              {
+                text: "介绍",
+                link: "/mist/introduce"
+              },
+              {
+                text: "脚手架",
+                link: "/mist/cli/"
+              },
+              {
+                text: "组件库",
+                link: "/mist/ui/"
+              }
+            ]
+          }
         ],
         sidebar: {
           "/": [
